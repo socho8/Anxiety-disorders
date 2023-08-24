d79fc0a12bb1de914a1ad845bf8d9f23554883bb
socho8 <alejosotillo63@gmail.com>
2023-08-24 14:22:55 +0200

    sync

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 
